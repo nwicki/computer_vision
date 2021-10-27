e5a82c1e5877f38caa66a99f5d85f2821296307f
Nicolas <nwicki@ethz.ch>
2021-10-27 10:49:25 +0200

    Assignment 2 - Base

---
+++ code/assignment2/mean-shift_cow/mean-shift.py
@@ -0,0 +1,72 @@
+import time
+import os
+import random
+import math
+import torch
+import numpy as np
+
+# run `pip install scikit-image` to install skimage, if you haven't done so
+from skimage import io, color
+from skimage.transform import rescale
+
+def distance(x, X):
+    res = torch.linalg.norm(X - x, dim=1)
+    return res
+
+def distance_batch(x, X):
+    raise NotImplementedError('distance_batch function not implemented!')
+
+def gaussian(dist, bandwidth):
+    res = torch.exp(-dist**2/(2*bandwidth**2))
+    return res
+
+def update_point(weight, X):
+    res = sum(weight[:, None] * X) / sum(weight)
+    return res
+
+def update_point_batch(weight, X):
+    raise NotImplementedError('update_point_batch function not implemented!')
+
+def meanshift_step(X, bandwidth=2.5):
+    X_ = X.clone()
+    for i, x in enumerate(X):
+        dist = distance(x, X)
+        weight = gaussian(dist, bandwidth)
+        X_[i] = update_point(weight, X)
+    return X_
+
+def meanshift_step_batch(X, bandwidth=2.5):
+    raise NotImplementedError('meanshift_step_batch function not implemented!')
+
+def meanshift(X):
+    X = X.clone()
+    for _ in range(1):
+        X = meanshift_step(X)  # slow implementation
+        # X = meanshift_step_batch(X)  # fast implementation
+    return X
+
+scale = 0.25    # downscale the image to run faster
+
+# Load image and convert it to CIELAB space
+image = rescale(io.imread('cow.jpg'), scale, multichannel=True)
+image_lab = color.rgb2lab(image)
+shape = image_lab.shape  # record image shape
+image_lab = image_lab.reshape([-1, 3])  # flatten the image
+
+# Run your mean-shift algorithm
+t = time.time()
+X = meanshift(torch.from_numpy(image_lab)).detach().cpu().numpy()
+t = time.time() - t
+print('Elapsed time for mean-shift: {}'.format(t))
+
+# Load label colors and draw labels as an image
+colors = np.load('colors.npz')['colors']
+colors[colors > 1.0] = 1
+colors[colors < 0.0] = 0
+
+centroids, labels = np.unique((X / 4).round(), return_inverse=True, axis=0)
+
+result_image = colors[labels].reshape(shape)
+result_image = rescale(result_image, 1 / scale, order=0, multichannel=True)     # resize result image to original resolution
+result_image = (result_image * 255).astype(np.uint8)
+io.imsave('result.png', result_image)
