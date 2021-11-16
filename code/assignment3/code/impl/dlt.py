c090fedaa90adca1eb3390c43197b3184cbbee38
Nicolas Wicki <nwicki@ethz.ch>
2021-11-16 12:00:34 +0100

    Restart

--- code/assignment3/code/impl/dlt.py
+++ code/assignment3/code/impl/dlt.py
@@ -1,18 +1,14 @@
 import numpy as np
-from impl.util import MakeHomogeneous, HNormalize
-
 
 def BuildProjectionConstraintMatrix(points2D, points3D):
-    # TODO
-    # For each correspondence, build the two rows of the constraint matrix and stack them
 
-    num_corrs = points2D.shape[0]
-    constraint_matrix = np.zeros((num_corrs * 2, 12))
+  # TODO
+  # For each correspondence, build the two rows of the constraint matrix and stack them
 
-    for i in range(num_corrs):
-        xi, yi, wi = MakeHomogeneous(points2D[i])
-        X_it = MakeHomogeneous(points3D[i])
-        constraint_matrix[2 * i] = np.array([*[0, 0, 0, 0], *(-wi * X_it), *(yi * X_it)])
-        constraint_matrix[2 * i + 1] = np.array([*(wi * X_it), *[0, 0, 0, 0], *(-xi * X_it)])
+  num_corrs = points2D.shape[0]
+  constraint_matrix = np.zeros((num_corrs * 2, 12))
 
-    return constraint_matrix
+  for i in range(num_corrs):
+    # TODO Add your code here
+
+  return constraint_matrix
